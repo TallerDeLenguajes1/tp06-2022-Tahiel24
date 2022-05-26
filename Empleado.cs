38fc741d75e11c6d6e1e93a34fa88b21da766ce6
Tahiel24 <tahielvarela@gmail.com>
2022-05-26 13:12:47 -0300

    Primeros avances del Ejercicio 2

---
+++ Empleado.cs
@@ -0,0 +1,112 @@
+public class Empleado{
+    //Declaracion de variables
+    private string nombre;
+    private string Apellido;
+    private DateTime FechaNacimiento;
+    private char estadoCivil;
+    private char genero;
+    private DateTime fechaIngreso;
+    private double sueldoBasico;
+    public enum Cargo{Auxiliar,Administrativo, Ingeniero, Especialista, Investigador}
+
+    //Propiedades
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Apellido1 { get => Apellido; set => Apellido = value; }
+    public DateTime FechaNacimiento1 { get => FechaNacimiento; set => FechaNacimiento = value; }
+    public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+    public char Genero { get => genero; set => genero = value; }
+    public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+    public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
+
+    //Constructor
+    public Empleado(DateTime fecha,string name, string ape,char est,char gen,DateTime fechaIng,double sueldoB)
+    {
+        FechaNacimiento1=fecha;
+        Apellido1=ape;
+        Nombre=name;
+        EstadoCivil=est;
+        Genero=gen;
+        FechaIngreso=fechaIng;
+        SueldoBasico=sueldoB;
+    }
+
+    //Funciones
+    public int Antiguedad(){
+        DateTime fecha;
+        int ao,actual,resultado;
+        fecha=fechaIngreso;
+        ao=Convert.ToInt32(fecha.Year);
+        actual=Convert.ToInt32(DateTime.Now.Year);
+        resultado=actual-ao;
+        return resultado;
+    }
+
+    public int Edad(){
+        DateTime fecha;
+        int ao,actual,resultado;
+        fecha=FechaNacimiento;
+        ao=Convert.ToInt32(fecha.Year);
+        actual=Convert.ToInt32(DateTime.Now.Year);
+        resultado=actual-ao;
+        return resultado;
+    }
+
+    public int EdadJub(int edadAcT){
+    int JubH=65,JubM=60,resta=0;
+        switch(genero){
+            case 'H':
+                resta=JubH-edadAcT;
+                break;
+            case 'M':
+                resta=JubM-edadAcT;
+                break;
+        }
+        return resta;
+    }
+    
+    public double SalarioTotal(int ant,string cargoEl){
+        double adicional=0,total,aux1,aux2;
+        int porcentaje,porcentaje2;
+        //Adicional para Antiguedad
+        if(ant<20){
+            porcentaje=ant;
+            aux1=(sueldoBasico*porcentaje)/100;
+            adicional+=aux1;
+        }else{
+            porcentaje=25;
+            aux1=(sueldoBasico*porcentaje)/100;
+            adicional+=aux1;
+        }
+        //Adicional segun cargo
+        switch(cargoEl){
+            case "Ingeniero":
+                porcentaje2=50;
+                aux2=(adicional*porcentaje2)/100;
+                adicional+=aux2;
+                break;
+            case "Especialista":
+                porcentaje2=50;
+                aux2=(adicional*porcentaje2)/100;
+                adicional+=aux2;
+                break;
+        }
+        //Adicional segun estado civil
+        if(estadoCivil=='C'){
+            adicional+=15000;
+        }
+        //Monto total
+        total=sueldoBasico+adicional;
+        return total;
+        
+    }
+
+}
+
+
+
+
+  
+
+
+    
+
--- Program.cs
+++ Program.cs
@@ -1,53 +1,16 @@
-﻿
-Console.WriteLine("Ingrese un numero: ");
-double numero = Convert.ToDouble(Console.ReadLine());
-Calculadora nuevaCalc = new Calculadora(numero);
-Console.WriteLine("\nSeleccione una operacion: ");
-Console.WriteLine("\n1. Suma");
-Console.WriteLine("\n2. Resta");
-Console.WriteLine("\n3. Producto");
-Console.WriteLine("\n4. Cociente");
-Console.WriteLine("\n5. Limpiar");
-Console.WriteLine("\n6. Salir");
-int op = Convert.ToInt32(Console.ReadLine());
-
-while (op != 6)
-{
-    switch (op)
-    {
-        case 1:
-            Console.WriteLine("Numero que desea sumar: ");
-            nuevaCalc.Sumar(Convert.ToDouble(Console.ReadLine()));
-            Console.WriteLine("Resultado: " + nuevaCalc.Resultado);
-            break;
-        case 2:
-            Console.WriteLine("Numero que desea restar: ");
-            nuevaCalc.Sumar(Convert.ToDouble(Console.ReadLine()));
-            Console.WriteLine("Resultado: " + nuevaCalc.Resultado);
-            break;
-        case 3:
-            Console.WriteLine("Numero que desea multiplicar: ");
-            nuevaCalc.Sumar(Convert.ToDouble(Console.ReadLine()));
-            Console.WriteLine("Resultado: " + nuevaCalc.Resultado);
-            break;
-        case 4:
-            Console.WriteLine("Numero que desea dividir: ");
-            nuevaCalc.Sumar(Convert.ToDouble(Console.ReadLine()));
-            Console.WriteLine("Resultado: " + nuevaCalc.Resultado);
-            break;
-        case 5:
-            nuevaCalc.Limpiar();
-            Console.WriteLine("Limpiado");
-            break;
-        case 6:
-            Console.WriteLine("Saliendo del programa");
-            break;
-    }
-   
-    Console.WriteLine("\nIngrese un numero del menu para seguir operando: ");
-    op = Convert.ToInt32(Console.ReadLine());
-}
-Console.WriteLine("\nPrograma finalizado");
-
-//Segundo ejercicio 
-
+﻿Console.WriteLine("----------------Primer empleado---------------");
+Console.WriteLine("Nombre: ");
+string nom=Console.ReadLine();
+Console.WriteLine("Apellido: ");
+string ape=Console.ReadLine();
+Console.WriteLine("Fecha de nacimiento: ");
+DateTime fechaNac= Convert.ToDateTime(Console.ReadLine());
+Console.WriteLine("Estado civil(C:Casado,S:Soltero,D:Divorciado,V:Viudo): ");
+char estC=Convert.ToChar(Console.ReadLine());
+Console.WriteLine("Genero(H:Hombre, M:Mujer, O:Otro): ");
+char gen=Convert.ToChar(Console.ReadLine());
+Console.WriteLine("Fecha de ingreso a la empresa: ");
+DateTime fechaIng=Convert.ToDateTime(Console.ReadLine());
+Console.WriteLine("Sueldo basico: ");
+double sueldo=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingrese su cargo: 0 = (Auxliar ) / 1  = (Administrativo) // 2 = (Ingeniero) // 3 = (Especialista) // 4 = (Investigador) ");
